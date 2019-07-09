f26d0413db2cbafbde06a2d22d8dde81a2f7cc5d
malavika <malu.sunflower@gmail.com>
2019-07-09 11:29:23 +0530

    Update compare-nonrpf-tunnel-id-cnt.py

--- kernel-tunnel/compare-nonrpf-tunnel-id-cnt.py
+++ kernel-tunnel/compare-nonrpf-tunnel-id-cnt.py
@@ -10,9 +10,9 @@ def compare_nonrpf_tunnel_id_cnt(nonrpf_tunnel_id_cnt, nonrpf_tunnel_id_max, **k
         val = 0
         low_thresh = decimal.Decimal(0.85)*nonrpf_tunnel_id_max
         high_thresh = decimal.Decimal(0.99)*nonrpf_tunnel_id_max
-        if nonrpf_tunnel_id_cnt <= low_thresh:
+        if nonrpf_tunnel_id_cnt < low_thresh:
             val = 0
-        elif nonrpf_tunnel_id_cnt > low_thresh  and  nonrpf_tunnel_id_cnt <= high_thresh:
+        elif nonrpf_tunnel_id_cnt >= low_thresh  and  nonrpf_tunnel_id_cnt <= high_thresh:
             val = 1
         elif nonrpf_tunnel_id_cnt > high_thresh:
             val = 2
