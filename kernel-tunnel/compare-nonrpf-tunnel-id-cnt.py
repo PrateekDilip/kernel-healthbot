be971bcd01f4c61546d0e0eb522a980a39c5c78a
malavika <malu.sunflower@gmail.com>
2019-07-11 16:55:33 +0530

    Update compare-nonrpf-tunnel-id-cnt.py

--- kernel-tunnel/compare-nonrpf-tunnel-id-cnt.py
+++ kernel-tunnel/compare-nonrpf-tunnel-id-cnt.py
@@ -1,6 +1,5 @@
 from jnpr.junos import Device
 from jnpr.junos.utils.config import Config
-import decimal
 
 """
 This function compares the current nonrpf tunnel id count in the system with the low threshold and high threshold and returns the result
@@ -8,8 +7,8 @@ This function compares the current nonrpf tunnel id count in the system with the
 
 def compare_nonrpf_tunnel_id_cnt(nonrpf_tunnel_id_cnt, nonrpf_tunnel_id_max, **kwargs):
         val = 0
-        low_thresh = decimal.Decimal(0.85)*nonrpf_tunnel_id_max
-        high_thresh = decimal.Decimal(0.99)*nonrpf_tunnel_id_max
+        low_thresh = float(0.85)*nonrpf_tunnel_id_max
+        high_thresh = float(0.99)*nonrpf_tunnel_id_max
         if nonrpf_tunnel_id_cnt < low_thresh:
             val = 0
         elif nonrpf_tunnel_id_cnt >= low_thresh  and  nonrpf_tunnel_id_cnt <= high_thresh:
