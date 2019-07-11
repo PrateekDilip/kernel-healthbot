d3969d7649a5df2600bbb7da9e3512aab9e81fdb
malavika <malu.sunflower@gmail.com>
2019-07-11 16:54:34 +0530

    Update compare-vpls-flood-token-cnt.py

--- kernel-vpls/compare-vpls-flood-token-cnt.py
+++ kernel-vpls/compare-vpls-flood-token-cnt.py
@@ -1,6 +1,5 @@
 from jnpr.junos import Device
 from jnpr.junos.utils.config import Config
-import decimal
 
 """
 This function compares the current vpls flood token count in the system with the low threshold and high threshold and returns the result
@@ -8,8 +7,8 @@ This function compares the current vpls flood token count in the system with the
 
 def compare_vpls_flood_token_cnt(vpls_flood_token_cnt, vpls_flood_token_max, **kwargs):
         val = 0
-        low_thresh = decimal.Decimal(0.8)*vpls_flood_token_max
-        high_thresh = decimal.Decimal(0.9)*vpls_flood_token_max
+        low_thresh = float(0.8)*vpls_flood_token_max
+        high_thresh = float(0.9)*vpls_flood_token_max
         if vpls_flood_token_cnt < low_thresh:
             val = 0
         elif vpls_flood_token_cnt >= low_thresh and vpls_flood_token_cnt <= high_thresh:
