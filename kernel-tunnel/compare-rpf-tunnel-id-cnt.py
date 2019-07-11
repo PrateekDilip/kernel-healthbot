c57d649ee5ade0946df158955bf774da5978f615
malavika <malu.sunflower@gmail.com>
2019-07-11 16:55:59 +0530

    Update compare-rpf-tunnel-id-cnt.py

--- kernel-tunnel/compare-rpf-tunnel-id-cnt.py
+++ kernel-tunnel/compare-rpf-tunnel-id-cnt.py
@@ -1,6 +1,5 @@
 from jnpr.junos import Device
 from jnpr.junos.utils.config import Config
-import decimal
 
 """
 This function compares the current rpf tunnel id count in the system with the low threshold and high threshold and returns the result
@@ -8,8 +7,8 @@ This function compares the current rpf tunnel id count in the system with the lo
 
 def compare_rpf_tunnel_id_cnt(rpf_tunnel_id_cnt, rpf_tunnel_id_max, **kwargs):
         val = 0
-        low_thresh = decimal.Decimal(0.8)*rpf_tunnel_id_max
-        high_thresh = decimal.Decimal(0.95)*rpf_tunnel_id_max
+        low_thresh = float(0.8)*rpf_tunnel_id_max
+        high_thresh = float(0.95)*rpf_tunnel_id_max
         if rpf_tunnel_id_cnt < low_thresh:
             val = 0
         elif rpf_tunnel_id_cnt >= low_thresh  and  rpf_tunnel_id_cnt <= high_thresh:
